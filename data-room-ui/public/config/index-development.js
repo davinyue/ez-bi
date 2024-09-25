03785c62f3cf0933f789c48d22bad1db0bd017bd
davin <davinyue@outlook.com>
2024-09-25 14:48:48 +0800

    feat: 更新配置

--- data-room-ui/public/config/index-development.js
+++ data-room-ui/public/config/index-development.js
@@ -8,8 +8,8 @@
 
 window.ENV = 'development'
 var developmentConfig = {
-  baseURL: 'http://127.0.0.1:8083/dataRoomServer',
-  fileUrlPrefix: 'http://127.0.0.1:8083/dataRoomServer' + '/static'
+  baseURL: 'http://127.0.0.1:8081',
+  fileUrlPrefix: 'http://127.0.0.1:8081' + '/static'
 }
 window.SITE_CONFIG.dataRoom = {}
 window.SITE_CONFIG.dataRoom  = configDeepMerge(window.SITE_CONFIG.dataRoom , developmentConfig)
